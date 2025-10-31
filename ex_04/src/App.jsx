b2c186252c410b0276b74101cdf93b69911f1a9d
JuandavidDuque166 <juandaduque880@gmail.com>
2025-10-31 10:04:20 -0500

    re act

---
+++ ex_04/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const App = () => {
+  return (
+    <return>
+      <>
+        <h1>Hello, world!</h1>
+        <p>Welcome to my React application.</p>
+      </>
+
+    </return>
+  )
+}
