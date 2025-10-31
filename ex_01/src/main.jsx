5655965e9c096229e46205b7da11044b5150f3dc
JuandavidDuque166 <juandaduque880@gmail.com>
2025-10-31 11:12:16 -0500

    Re act2

--- ex_01/src/main.jsx
+++ ex_01/src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-
-import { PrimerComponente } from './PrimerComponente'
+import './index.css'
+import { App } from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <PrimerComponente />
+    <App />
   </StrictMode>,
 )
